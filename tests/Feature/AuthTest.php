49f6946107682c5fd51904c4e64991415f1529e2
Andrey Petrov <andyandex@mail.ru>
2020-12-15 17:15:55 +0400

    Final

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -16,12 +16,26 @@ class AuthTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testRegister()
     {
         $response = $this->get('/register');
         $response->assertStatus(200);
         $response->assertSee('Регистрация');
+        $response->assertSee('Имя');
+        $response->assertSee('Фамилия');
+        $response->assertSee('Телефон');
         $response->assertSee('Фамилия пригласившего');
+        $response->assertSee('Организация');
+        $response->assertSee('Пароль');
+        $response->assertSee('Подтверждение пароля');
 
+    }
+    public function testlogin()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+        $response->assertSee('Вход');
+        $response->assertSee('Телефон');
+        $response->assertSee('Пароль');
     }
 }
